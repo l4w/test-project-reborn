7cb3f9b42488c113926fe035e66bd255d86d34b8
l4w <lawzdan@gmail.com>
2021-03-19 01:12:57 +0200

    Commit #2 18.03.2021
    +ADD locators.py; +UPD main/login Pages with new locators; +UPD login_page w/ new expanded methods to check presence on the login page.

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,20 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
 
     def should_be_login_page(self):
-        self.browser.find_element(By.CSS_SELECTOR, '[name="login_submit"]')
+        self.should_be_login_page_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_page_url(self):
+        assert 'login' in self.browser.current_url, 'Check URL of your login page.'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'No login form on the page.'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'No register form on the page.'
 
